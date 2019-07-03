b5f236a902ad85d5cf9fa4f17187f1f514332629
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-03 10:17:43 +0530

    Create services.component.ts

---
+++ src/app/services/services.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  template: `
+    <p>
+    services work!
+    </p>
+  `,
+  styles: []
+})
+export class ServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
