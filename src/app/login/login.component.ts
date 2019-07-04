9722c15af03e287d7448f5cb7d32e36fe699894d
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-04 13:01:37 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, CheckboxControlValueAccessor}  from '@angular/forms'
-import { NgModel} from '@angular/forms'
-import { from } from 'rxjs';
-import { getAttrsForDirectiveMatching } from '@angular/compiler/src/render3/view/util';
+
 @Component({
   selector: 'app-login',
   templateUrl:`./login.component.html` ,
@@ -15,17 +12,10 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
-   check(){
-    var output = this.username.length
-    var output = this.password.length
-      {
-        if(this.username.length == 5 && this.password.length== 5 )
-        {
-          return "welcome"
-        }
-      }
-   }
+}
+      
+   
 
    
   
-    }
+    
