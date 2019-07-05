dd79232e1633d8def0e0720aa9b783b9a4f14d37
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-05 17:57:26 +0530

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'\
+import { HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService{
-private _url: string = '/src/app/test/file.json';
+//private _url: string = '/src/app/test/file.json';
 constructor( private http : HttpClient){}
- getdata(){
-    
+ fetchAuth(): Observable<object>{
+    return this.http.get('assests/Data/file.json');
  }
 }
