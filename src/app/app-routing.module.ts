e5f981dffa2e5349999ed9de79b772b5798dad1e
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-05 14:16:25 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ServicesComponent } from './services/services.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},
+  {path:'login', component: LoginComponent, },
   {path:'services', component : ServicesComponent}  
 ];
 
