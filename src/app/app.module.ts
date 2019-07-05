1feeb92e563b62d426ddae6443f6a9c586b7582c
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-05 17:57:22 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ServicesComponent } from './services/services.component';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthService} from './auth.service';
+//import { AuthService} from './auth.service';
 //import { AuthGuard} from './auth.guard'
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ServicesComponent,
-    AuthService
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    AuthService
+    //AuthService
   ],
-  providers: [AuthService],
-  bootstrap: [AppComponent]
+  providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
