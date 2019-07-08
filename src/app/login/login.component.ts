d2d307b460c69bf044d0627618aa6c035ce81562
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-08 12:21:50 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl:`./login.component.html` ,
+  template:`<h2> {{"hello" +Ydata}}</h2>
+            <button (click)="FireEvent()">Send Event</button>` ,
   styles: []
 })
 export class LoginComponent implements OnInit {
 
   inputText: string="inital value"
   inputText2:string =""
-
+ @Input() public Ydata;
+ @Output() public loginEvent = new EventEmitter
   constructor() { }
 
   ngOnInit() {
-   localStorage.setItem('localData', 'yash');
-   sessionStorage.setItem('sessionData', 'yash2');
+   //localStorage.setItem('localData', 'yash');
+   //sessionStorage.setItem('sessionData', 'yash2');
 
-   console.log(localStorage.getItem('localData'));
-  console.log(sessionStorage.getItem('sessionData'));
+   //console.log(localStorage.getItem('localData'));
+  //console.log(sessionStorage.getItem('sessionData'));
+  }
+  FireEvent()
+  {
+    this.loginEvent.emit('hello yash');
   }
   
 }
