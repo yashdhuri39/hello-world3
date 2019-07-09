bcde6c6272abf7b7b3f193c35088c82b856d1544
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-09 12:10:56 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService} from './auth.service';
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { IntercomponentService } from './intercomponent.service';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,12 @@ export class AppComponent {
   data$;
   public Name ="Yash";
   public message= "";
-  constructor( private Authservice : AuthService){
+  constructor( private Authservice : AuthService, private intercomponent : IntercomponentService){
+  
 
+  } 
+  sayhello(){
+    this.intercomponent.sendmessage('hello aditya');
   }
   
   fetchAuth(){
