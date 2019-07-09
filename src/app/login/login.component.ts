12d5e6823961337cf5a47ead1a18fe637f75cd40
yashdhuri39 <yashdhuri39@gmail.com>
2019-07-09 12:10:44 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IntercomponentService } from '../intercomponent.service';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +13,27 @@ export class LoginComponent implements OnInit {
   //inputText2:string =""
  @Input() public Ydata;
  @Output() public loginEvent = new EventEmitter
-  constructor() { }
+  constructor(private intercomponent: IntercomponentService) { }
 
   ngOnInit() {
+    this.intercomponent.message1$
+    .subscribe(
+      Message=>{
+        if (Message === 'hello aditya'){
+          alert( 'welcome Aditya');
+        }
+      }
+    )
    //localStorage.setItem('localData', 'yash');
    //sessionStorage.setItem('sessionData', 'yash2');
 
    //console.log(localStorage.getItem('localData'));
   //console.log(sessionStorage.getItem('sessionData'));
   }
-  FireEvent()
-  {
-    this.loginEvent.emit('hello yash');
-  }
+  //FireEvent()
+  //{
+    //this.loginEvent.emit('hello yash');
+  //}
 
 }
       
